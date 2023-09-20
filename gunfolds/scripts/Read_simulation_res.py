96bcc600a71b700196526e932ac217c7c5565ecc
sajadabvi <abavisani@me.com>
2023-09-20 16:06:55 -0400

    new plotting script

--- gunfolds/scripts/Read_simulation_res.py
+++ gunfolds/scripts/Read_simulation_res.py
@@ -5,7 +5,9 @@ from matplotlib import pyplot as plt
 from matplotlib.ticker import MultipleLocator
 
 folder = '/Users/sajad/Code_local/mygit/gunfolds/gunfolds/scripts/results/VAR_simulation_results/res_simulation' \
-         '/7nodes/zkl/'
+         '/8nodes/n8stmt14/'
+folder2 = '/Users/sajad/Code_local/mygit/gunfolds/gunfolds/scripts/results/VAR_simulation_results/res_simulation' \
+         '/8nodes/n8stmf14/'
 cats = [1.49, 47.566, 180.738, 20.914, 132.105, 31.86, 55.764, 39.024, 6.292, 26.122, 0.979, 0.727, 4.468, 20.192,
         53.597, 0.938, 0.477, 30.932, 0.148, 0.577, 0.094, 19.898, 54.733, 65.322, 23.107, 26.225, 48.217, 0.802,
         45.076, 0.763]
@@ -20,12 +22,18 @@ file_list.sort()
 if file_list[0].startswith('.'):
     file_list.pop(0)
 
+file_list2 = listdir(folder2)
+file_list2.sort()
+if file_list2[0].startswith('.'):
+    file_list2.pop(0)
+
 # activate this part if time information is not in .zkl files and manually add them through [cats]
 '''# for i in range(30):
 #     item = zkl.load(folder + file_list[i])
 #     item['mins'] = cats[i]
 #     zkl.save(item, folder + file_list[i])'''
 res = [zkl.load(folder + file) for file in file_list]
+res2 = [zkl.load(folder2 + file) for file in file_list2]
 
 G1_opt_error_GT_om = []
 G1_opt_error_GT_com = []
@@ -34,6 +42,7 @@ Gu_opt_errors_network_GT_U_com = []
 Gu_opt_errors_g_estimated_om = []
 Gu_opt_errors_g_estimated_com = []
 
+times = []
 for item in res:
     G1_opt_error_GT_om.append(item['G1_opt_error_GT'][0])
     G1_opt_error_GT_com.append(item['G1_opt_error_GT'][1])
@@ -44,32 +53,51 @@ for item in res:
     Gu_opt_errors_g_estimated_om.append(item['Gu_opt_errors_g_estimated'][0])
     Gu_opt_errors_g_estimated_com.append(item['Gu_opt_errors_g_estimated'][1])
 
+    times.append(item['total_time'])
+
+for item in res2:
+    G1_opt_error_GT_om.append(item['G1_opt_error_GT'][0])
+    G1_opt_error_GT_com.append(item['G1_opt_error_GT'][1])
+
+    Gu_opt_errors_network_GT_U_om.append(item['Gu_opt_errors_network_GT_U'][0])
+    Gu_opt_errors_network_GT_U_com.append(item['Gu_opt_errors_network_GT_U'][1])
+
+    Gu_opt_errors_g_estimated_om.append(item['Gu_opt_errors_g_estimated'][0])
+    Gu_opt_errors_g_estimated_com.append(item['Gu_opt_errors_g_estimated'][1])
+
+    times.append(item['total_time'])
+
+min_value = min(times)
+max_value = max(times)
+
+normalized_times = [(x - min_value) / (max_value - min_value) for x in times]
+
 sns.set_context("notebook", font_scale=1.5, rc={"lines.linewidth": 1.75})
 sns.set_style("darkgrid")
 # Create a figure with two subplots
 fig, axes = plt.subplots(2, 1, figsize=(10, 8))  # 2 rows, 1 column
 
 # First subplot
-sns.lineplot(data=G1_opt_error_GT_om, label='G1_opt error GT', ax=axes[0])
+# sns.lineplot(data=G1_opt_error_GT_om, label='G1_opt error GT', ax=axes[0])
 sns.lineplot(data=Gu_opt_errors_network_GT_U_om, label='Gu_opt errors network_GT_U', ax=axes[0])
-sns.lineplot(data=Gu_opt_errors_g_estimated_om, label='Gu_opt errors g_estimated', ax=axes[0])
-sns.lineplot(data=normalized_cats, label='normalized time', ax=axes[0])
-axes[0].set_title('VAR Simulation on 7 node random graphs error for different SCC options')
-axes[0].set_xlim(0, 29)
+# sns.lineplot(data=Gu_opt_errors_g_estimated_om, label='Gu_opt errors g_estimated', ax=axes[0])
+sns.lineplot(data=normalized_times, label='normalized time', ax=axes[0])
+axes[0].set_title('VAR Simulation on 8 node random graphs error for different SCC options')
+axes[0].set_xlim(0, 69)
 axes[0].set_ylim(0, 1)
 
 # Second subplot
-sns.lineplot(data=G1_opt_error_GT_com, label='G1 opt error GT', ax=axes[1])
+# sns.lineplot(data=G1_opt_error_GT_com, label='G1 opt error GT', ax=axes[1])
 sns.lineplot(data=Gu_opt_errors_network_GT_U_com, label='Gu_opt errors network_GT_U', ax=axes[1])
-sns.lineplot(data=Gu_opt_errors_g_estimated_com, label='Gu_opt errors g_estimated', ax=axes[1])
-axes[1].set_xlim(0, 29)
+# sns.lineplot(data=Gu_opt_errors_g_estimated_com, label='Gu_opt errors g_estimated', ax=axes[1])
+axes[1].set_xlim(0, 69)
 axes[1].set_ylim(0, 1)
 
 for ax in axes:
     ax.xaxis.grid(True, "minor", linewidth=.75)
     ax.xaxis.grid(True, "major", linewidth=5)
     ax.xaxis.set_minor_locator(MultipleLocator(1))
-    ax.xaxis.set_major_locator(MultipleLocator(10))
+    ax.xaxis.set_major_locator(MultipleLocator(33))
 
 plt.show()
-# plt.savefig("simulation_results_7nodes.svg")
+# plt.savefig("simulation_results_8nodes.svg")
---
+++ gunfolds/scripts/plot_configuration_test.py
@@ -0,0 +1,75 @@
+from matplotlib import pyplot as plt
+import pandas as pd
+import seaborn as sns
+from gunfolds.utils import zickle as zkl
+from os import listdir
+from matplotlib.patheffects import withStroke
+from matplotlib.ticker import MultipleLocator
+
+list_of_lists1 = []
+list_of_lists2 = []
+folder = '/Users/sajad/Code_local/mygit/gunfolds/gunfolds/scripts/results/test_configuration_options/zkl'
+directories = listdir(folder)
+directories.sort()
+if directories[0].startswith('.'):
+    directories.pop(0)
+
+for directory in directories:
+    file_list1 = listdir(f'{folder}/{directory}/7')
+    file_list1.sort()
+    if file_list1[0].startswith('.'):
+        file_list1.pop(0)
+    item_list = [zkl.load(f'{folder}/{directory}/7/{name}') for name in file_list1]
+    list_of_lists1.append(item_list)
+
+
+for directory in directories:
+    file_list1 = listdir(f'{folder}/{directory}/15')
+    file_list1.sort()
+    if file_list1[0].startswith('.'):
+        file_list1.pop(0)
+    item_list = [zkl.load(f'{folder}/{directory}/15/{name}') for name in file_list1]
+    list_of_lists2.append(item_list)
+
+if __name__ == '__main__':
+    df = pd.DataFrame()
+    Err = []
+    config = []
+    time = []
+    pnum = []
+    for index, item_list in enumerate(list_of_lists1):
+        for item in item_list:
+            time.append(item['total_time'])
+            config.append(item['config'])
+            Err.append((item['Gu_opt_errors_network_GT_U'][0] + item['Gu_opt_errors_network_GT_U'][1])/2)
+            pnum.append(7)
+
+    # for index, item_list in enumerate(list_of_lists2):
+    #     for item in item_list:
+    #         time.append(item['total_time'])
+    #         config.append(item['config'])
+    #         Err.append((item['Gu_opt_errors_network_GT_U'][0] + item['Gu_opt_errors_network_GT_U'][1])/2)
+    #         pnum.append(15)
+
+
+# Create a new DataFrame
+data = pd.DataFrame({'config': config, 'Err': Err, 'pnum': pnum, 'time':time})
+
+# Create a box plot using Seaborn
+plt.figure(figsize=(10, 6))  # Adjust figure size if needed
+sns.boxplot(x='config', y='time', hue='config', data=data, palette='Set1')
+
+# Set labels and title
+plt.xlabel('Configuration')
+plt.ylabel('time')
+plt.title('Box Plot of time by Configuration')
+plt.ylim(0.05, 5000)
+plt.yscale('log')  # Set the y-axis scale to log
+
+
+# Add legend
+plt.legend(title='Config', loc='upper right')
+
+# Show the
+plt.savefig("only7_cor.svg")
+# plt.show()
