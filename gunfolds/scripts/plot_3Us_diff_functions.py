5f81235b5514a042a2614e0e84e2fa6ba4885a8e
sajadabvi <abavisani@me.com>
2024-03-07 19:40:13 -0500

    added function to compute frequency of edges error

--- gunfolds/scripts/plot_3Us_diff_functions.py
+++ gunfolds/scripts/plot_3Us_diff_functions.py
@@ -8,39 +8,6 @@ from collections import defaultdict
 from gunfolds.utils import graphkit as gk
 from gunfolds.utils import bfutils
 
-def merge_graphs(graphs, threshold_density):
-    # Dictionary to store edge frequencies
-    edge_frequencies = defaultdict(int)
-
-    # Set to store all nodes in the input graphs
-    all_nodes = set()
-
-    # Count the frequencies of each edge in the list of graphs
-    for graph in graphs:
-        all_nodes.update(graph.keys())
-        for node, connections in graph.items():
-            for neighbor, edge_type in connections.items():
-                edge_frequencies[(node, neighbor, edge_type)] += 1
-
-    # Calculate the threshold for inclusion based on the provided density
-    threshold = threshold_density * len(graphs)
-
-    # Create the merged graph based on edges that meet the threshold
-    merged_graph = {}
-    for (node, neighbor, edge_type), frequency in edge_frequencies.items():
-        if frequency >= threshold:
-            if node not in merged_graph:
-                merged_graph[node] = {}
-            merged_graph[node][neighbor] = edge_type
-
-    # Ensure all nodes are present in the merged graph
-    for graph in graphs:
-        for node in all_nodes:
-            if node not in merged_graph:
-                merged_graph[node] = {}
-
-    return merged_graph
-
 
 ############################################################
 
@@ -117,22 +84,80 @@ Gu_opt_errors_g_estimated_om = []
 Gu_opt_errors_g_estimated_com = []
 
 err_criteria = 'total'
+def merge_graphs(graphs, threshold_density):
+    # Dictionary to store edge frequencies
+    edge_frequencies = defaultdict(int)
+
+    # Set to store all nodes in the input graphs
+    all_nodes = set()
+
+    # Count the frequencies of each edge in the list of graphs
+    for graph in graphs:
+        all_nodes.update(graph.keys())
+        for node, connections in graph.items():
+            for neighbor, edge_type in connections.items():
+                edge_frequencies[(node, neighbor, edge_type)] += 1
+
+    # Calculate the threshold for inclusion based on the provided density
+    threshold = threshold_density * len(graphs)
+
+    # Create the merged graph based on edges that meet the threshold
+    merged_graph = {}
+    for (node, neighbor, edge_type), frequency in edge_frequencies.items():
+        if frequency >= threshold:
+            if node not in merged_graph:
+                merged_graph[node] = {}
+            merged_graph[node][neighbor] = edge_type
+
+    # Ensure all nodes are present in the merged graph
+    for graph in graphs:
+        for node in all_nodes:
+            if node not in merged_graph:
+                merged_graph[node] = {}
+
+    return merged_graph
+
+def cal_freq_error(item, err_criteria, type):
+    g1s = []
+    gus = []
+    gt = item['general']['GT']
+    gtu = item['general']['GT_at_actual_U']
+    g_est = item['general']['g_estimated']
+    for sol in item['general']['full_sols']:
+        g1s.append(bfutils.num2CG(sol[0][0], len(item['general']['GT'])))
+        gus.append(bfutils.undersample(bfutils.num2CG(sol[0][0], len(item['general']['GT'])), sol[0][1][0]))
+    th1 = 0.1
+    g1 = merge_graphs(g1s,th1)
+    while gk.density(g1) > gk.density(gt):
+        th1 += 0.01
+        g1 = merge_graphs(g1s, th1)
+
+    thu = 0.1
+    gu = merge_graphs(gus, thu)
+    while gk.density(gu) > gk.density(gtu):
+        thu += 0.01
+        gu = merge_graphs(gus, thu)
+
+    err_GuVsGTu = gk.OCE(gu, gtu, undirected=False, normalized=True)[err_criteria][type]
+    err_GuVsGest = gk.OCE(gu, g_est, undirected=False, normalized=True)[err_criteria][type]
+    err_G1VsGT = gk.OCE(g1, gt, undirected=False, normalized=True)[err_criteria][type]
+    return [err_GuVsGTu, err_GuVsGest, err_G1VsGT]
 def cal_mean_error(item, err_criteria, type):
     GuVsGTu = []
     GuVsGest = []
     G1VsGT = []
+    gt = item['general']['GT']
+    gtu = item['general']['GT_at_actual_U']
+    g_est = item['general']['g_estimated']
     for sol in item['general']['full_sols']:
         g1 = bfutils.num2CG(sol[0][0], len(item['general']['GT']))
         gu = bfutils.undersample(bfutils.num2CG(sol[0][0], len(item['general']['GT'])), sol[0][1][0])
-        gt = item['general']['GT']
-        gtu = item['general']['GT_at_actual_U']
-        g_est = item['general']['g_estimated']
         GuVsGTu.append(gk.OCE(gu, gtu, undirected=False, normalized=True)[err_criteria][type])
         GuVsGest.append(gk.OCE(gu, g_est, undirected=False, normalized=True)[err_criteria][type])
         G1VsGT.append(gk.OCE(g1, gt, undirected=False, normalized=True)[err_criteria][type])
-        err_GuVsGTu = sum(GuVsGTu) / len(GuVsGTu)
-        err_GuVsGest = sum(GuVsGest) / len(GuVsGest)
-        err_G1VsGT = sum(G1VsGT) / len(G1VsGT)
+    err_GuVsGTu = sum(GuVsGTu) / len(GuVsGTu)
+    err_GuVsGest = sum(GuVsGest) / len(GuVsGest)
+    err_G1VsGT = sum(G1VsGT) / len(G1VsGT)
     return [err_GuVsGTu, err_GuVsGest, err_G1VsGT]
 
 if __name__ == '__main__':
@@ -214,14 +239,14 @@ if __name__ == '__main__':
     for item in res17:
         ErrVs.extend(['GuVsGTu', 'GuVsGest', 'G1VsGT'])
         WRT.extend(['GuOptVsGest', 'GuOptVsGest', 'GuOptVsGest'])
-        Err.extend(cal_mean_error(item,err_criteria,0))
+        Err.extend(cal_freq_error(item,err_criteria,0))
         ErrType.extend(['omm', 'omm', 'omm'])
-        weights_scheme.extend(['U2_mean_error', 'U2_mean_error', 'U2_mean_error'])
+        weights_scheme.extend(['U2_freq_error', 'U2_freq_error', 'U2_freq_error'])
         ErrVs.extend(['GuVsGTu', 'GuVsGest', 'G1VsGT'])
         WRT.extend(['GuOptVsGest', 'GuOptVsGest', 'GuOptVsGest'])
-        Err.extend(cal_mean_error(item,err_criteria,1))
+        Err.extend(cal_freq_error(item,err_criteria,1))
         ErrType.extend(['comm', 'comm', 'comm'])
-        weights_scheme.extend(['U2_mean_error', 'U2_mean_error', 'U2_mean_error'])
+        weights_scheme.extend(['U2_freq_error', 'U2_freq_error', 'U2_freq_error'])
         #################################################################################################
         # ErrVs.extend(['GuVsGTu', 'GuVsGest', 'G1VsGT'])
         # WRT.extend(['GuOptVsGTu', 'GuOptVsGTu', 'GuOptVsGTu'])
@@ -229,25 +254,25 @@ if __name__ == '__main__':
         #             item['GuOptVsGTu']['Gu_opt_errors_g_estimated_WRT_GuOptVsGTu'][0],
         #             item['GuOptVsGTu']['G1_opt_error_GT_WRT_GuOptVsGTu'][0]])
         # ErrType.extend(['omm', 'omm', 'omm'])
-        # weights_scheme.extend(['U2_mean_error', 'U2_mean_error', 'U2_mean_error'])
+        # weights_scheme.extend(['U2_freq_error', 'U2_freq_error', 'U2_freq_error'])
         # ErrVs.extend(['GuVsGTu', 'GuVsGest', 'G1VsGT'])
         # WRT.extend(['GuOptVsGTu', 'GuOptVsGTu', 'GuOptVsGTu'])
         # Err.extend([item['GuOptVsGTu']['Gu_opt_errors_network_GT_U_WRT_GuOptVsGTu'][1],
         #             item['GuOptVsGTu']['Gu_opt_errors_g_estimated_WRT_GuOptVsGTu'][1],
         #             item['GuOptVsGTu']['G1_opt_error_GT_WRT_GuOptVsGTu'][1]])
         # ErrType.extend(['comm', 'comm', 'comm'])
-        # weights_scheme.extend(['U2_mean_error', 'U2_mean_error', 'U2_mean_error'])
+        # weights_scheme.extend(['U2_freq_error', 'U2_freq_error', 'U2_freq_error'])
         #################################################################################################
         ErrVs.extend(['GuVsGTu', 'GuVsGest', 'G1VsGT'])
         WRT.extend(['G1OptVsGT', 'G1OptVsGT', 'G1OptVsGT'])
-        Err.extend(cal_mean_error(item,err_criteria,0))
+        Err.extend(cal_freq_error(item,err_criteria,0))
         ErrType.extend(['omm', 'omm', 'omm'])
-        weights_scheme.extend(['U2_mean_error', 'U2_mean_error', 'U2_mean_error'])
+        weights_scheme.extend(['U2_freq_error', 'U2_freq_error', 'U2_freq_error'])
         ErrVs.extend(['GuVsGTu', 'GuVsGest', 'G1VsGT'])
         WRT.extend(['G1OptVsGT', 'G1OptVsGT', 'G1OptVsGT'])
-        Err.extend(cal_mean_error(item,err_criteria,1))
+        Err.extend(cal_freq_error(item,err_criteria,1))
         ErrType.extend(['comm', 'comm', 'comm'])
-        weights_scheme.extend(['U2_mean_error', 'U2_mean_error', 'U2_mean_error'])
+        weights_scheme.extend(['U2_freq_error', 'U2_freq_error', 'U2_freq_error'])
 
     for item in res15:
         ErrVs.extend(['GuVsGTu', 'GuVsGest', 'G1VsGT'])
@@ -312,14 +337,14 @@ if __name__ == '__main__':
     for item in res18:
         ErrVs.extend(['GuVsGTu', 'GuVsGest', 'G1VsGT'])
         WRT.extend(['GuOptVsGest', 'GuOptVsGest', 'GuOptVsGest'])
-        Err.extend(cal_mean_error(item,err_criteria,0))
+        Err.extend(cal_freq_error(item,err_criteria,0))
         ErrType.extend(['omm', 'omm', 'omm'])
-        weights_scheme.extend(['U3_mean_error', 'U3_mean_error', 'U3_mean_error'])
+        weights_scheme.extend(['U3_freq_error', 'U3_freq_error', 'U3_freq_error'])
         ErrVs.extend(['GuVsGTu', 'GuVsGest', 'G1VsGT'])
         WRT.extend(['GuOptVsGest', 'GuOptVsGest', 'GuOptVsGest'])
-        Err.extend(cal_mean_error(item,err_criteria,1))
+        Err.extend(cal_freq_error(item,err_criteria,1))
         ErrType.extend(['comm', 'comm', 'comm'])
-        weights_scheme.extend(['U3_mean_error', 'U3_mean_error', 'U3_mean_error'])
+        weights_scheme.extend(['U3_freq_error', 'U3_freq_error', 'U3_freq_error'])
         #################################################################################################
         # ErrVs.extend(['GuVsGTu', 'GuVsGest', 'G1VsGT'])
         # WRT.extend(['GuOptVsGTu', 'GuOptVsGTu', 'GuOptVsGTu'])
@@ -327,25 +352,25 @@ if __name__ == '__main__':
         #             item['GuOptVsGTu']['Gu_opt_errors_g_estimated_WRT_GuOptVsGTu'][0],
         #             item['GuOptVsGTu']['G1_opt_error_GT_WRT_GuOptVsGTu'][0]])
         # ErrType.extend(['omm', 'omm', 'omm'])
-        # weights_scheme.extend(['U3_mean_error', 'U3_mean_error', 'U3_mean_error'])
+        # weights_scheme.extend(['U3_freq_error', 'U3_freq_error', 'U3_freq_error'])
         # ErrVs.extend(['GuVsGTu', 'GuVsGest', 'G1VsGT'])
         # WRT.extend(['GuOptVsGTu', 'GuOptVsGTu', 'GuOptVsGTu'])
         # Err.extend([item['GuOptVsGTu']['Gu_opt_errors_network_GT_U_WRT_GuOptVsGTu'][1],
         #             item['GuOptVsGTu']['Gu_opt_errors_g_estimated_WRT_GuOptVsGTu'][1],
         #             item['GuOptVsGTu']['G1_opt_error_GT_WRT_GuOptVsGTu'][1]])
         # ErrType.extend(['comm', 'comm', 'comm'])
-        # weights_scheme.extend(['U3_mean_error', 'U3_mean_error', 'U3_mean_error'])
+        # weights_scheme.extend(['U3_freq_error', 'U3_freq_error', 'U3_freq_error'])
         #################################################################################################
         ErrVs.extend(['GuVsGTu', 'GuVsGest', 'G1VsGT'])
         WRT.extend(['G1OptVsGT', 'G1OptVsGT', 'G1OptVsGT'])
-        Err.extend(cal_mean_error(item,err_criteria,0))
+        Err.extend(cal_freq_error(item,err_criteria,0))
         ErrType.extend(['omm', 'omm', 'omm'])
-        weights_scheme.extend(['U3_mean_error', 'U3_mean_error', 'U3_mean_error'])
+        weights_scheme.extend(['U3_freq_error', 'U3_freq_error', 'U3_freq_error'])
         ErrVs.extend(['GuVsGTu', 'GuVsGest', 'G1VsGT'])
         WRT.extend(['G1OptVsGT', 'G1OptVsGT', 'G1OptVsGT'])
-        Err.extend(cal_mean_error(item,err_criteria,1))
+        Err.extend(cal_freq_error(item,err_criteria,1))
         ErrType.extend(['comm', 'comm', 'comm'])
-        weights_scheme.extend(['U3_mean_error', 'U3_mean_error', 'U3_mean_error'])
+        weights_scheme.extend(['U3_freq_error', 'U3_freq_error', 'U3_freq_error'])
 
     for item in res16:
         ErrVs.extend(['GuVsGTu', 'GuVsGest', 'G1VsGT'])
@@ -410,14 +435,14 @@ if __name__ == '__main__':
     for item in res19:
         ErrVs.extend(['GuVsGTu', 'GuVsGest', 'G1VsGT'])
         WRT.extend(['GuOptVsGest', 'GuOptVsGest', 'GuOptVsGest'])
-        Err.extend(cal_mean_error(item,err_criteria,0))
+        Err.extend(cal_freq_error(item,err_criteria,0))
         ErrType.extend(['omm', 'omm', 'omm'])
-        weights_scheme.extend(['U4_mean_error', 'U4_mean_error', 'U4_mean_error'])
+        weights_scheme.extend(['U4_freq_error', 'U4_freq_error', 'U4_freq_error'])
         ErrVs.extend(['GuVsGTu', 'GuVsGest', 'G1VsGT'])
         WRT.extend(['GuOptVsGest', 'GuOptVsGest', 'GuOptVsGest'])
-        Err.extend(cal_mean_error(item,err_criteria,1))
+        Err.extend(cal_freq_error(item,err_criteria,1))
         ErrType.extend(['comm', 'comm', 'comm'])
-        weights_scheme.extend(['U4_mean_error', 'U4_mean_error', 'U4_mean_error'])
+        weights_scheme.extend(['U4_freq_error', 'U4_freq_error', 'U4_freq_error'])
         #################################################################################################
         # ErrVs.extend(['GuVsGTu', 'GuVsGest', 'G1VsGT'])
         # WRT.extend(['GuOptVsGTu', 'GuOptVsGTu', 'GuOptVsGTu'])
@@ -425,25 +450,25 @@ if __name__ == '__main__':
         #             item['GuOptVsGTu']['Gu_opt_errors_g_estimated_WRT_GuOptVsGTu'][0],
         #             item['GuOptVsGTu']['G1_opt_error_GT_WRT_GuOptVsGTu'][0]])
         # ErrType.extend(['omm', 'omm', 'omm'])
-        # weights_scheme.extend(['U4_mean_error', 'U4_mean_error', 'U4_mean_error'])
+        # weights_scheme.extend(['U4_freq_error', 'U4_freq_error', 'U4_freq_error'])
         # ErrVs.extend(['GuVsGTu', 'GuVsGest', 'G1VsGT'])
         # WRT.extend(['GuOptVsGTu', 'GuOptVsGTu', 'GuOptVsGTu'])
         # Err.extend([item['GuOptVsGTu']['Gu_opt_errors_network_GT_U_WRT_GuOptVsGTu'][1],
         #             item['GuOptVsGTu']['Gu_opt_errors_g_estimated_WRT_GuOptVsGTu'][1],
         #             item['GuOptVsGTu']['G1_opt_error_GT_WRT_GuOptVsGTu'][1]])
         # ErrType.extend(['comm', 'comm', 'comm'])
-        # weights_scheme.extend(['U4_mean_error', 'U4_mean_error', 'U4_mean_error'])
+        # weights_scheme.extend(['U4_freq_error', 'U4_freq_error', 'U4_freq_error'])
         #################################################################################################
         ErrVs.extend(['GuVsGTu', 'GuVsGest', 'G1VsGT'])
         WRT.extend(['G1OptVsGT', 'G1OptVsGT', 'G1OptVsGT'])
-        Err.extend(cal_mean_error(item,err_criteria,0))
+        Err.extend(cal_freq_error(item,err_criteria,0))
         ErrType.extend(['omm', 'omm', 'omm'])
-        weights_scheme.extend(['U4_mean_error', 'U4_mean_error', 'U4_mean_error'])
+        weights_scheme.extend(['U4_freq_error', 'U4_freq_error', 'U4_freq_error'])
         ErrVs.extend(['GuVsGTu', 'GuVsGest', 'G1VsGT'])
         WRT.extend(['G1OptVsGT', 'G1OptVsGT', 'G1OptVsGT'])
-        Err.extend(cal_mean_error(item,err_criteria,1))
+        Err.extend(cal_freq_error(item,err_criteria,1))
         ErrType.extend(['comm', 'comm', 'comm'])
-        weights_scheme.extend(['U4_mean_error', 'U4_mean_error', 'U4_mean_error'])
+        weights_scheme.extend(['U4_freq_error', 'U4_freq_error', 'U4_freq_error'])
 
     df['Err'] = Err
     df['ErrVs'] = ErrVs
@@ -476,4 +501,4 @@ if __name__ == '__main__':
             ax.set_ylim(0, 1)
     plt.suptitle("calculating " + err_criteria + " errors ", x=0.45, y=1, fontsize=20)
     # plt.show()
-    plt.savefig("figs/old_errror_cal_VS_mean_error_" + err_criteria + "_error.png")
+    plt.savefig("figs/old_errror_cal_VS_frequency_error_" + err_criteria + "_error.png")
