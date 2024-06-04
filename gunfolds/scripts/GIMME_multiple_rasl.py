d3c8f255a4ad15cecf027f90c4dc5c42392ec7aa
sajadabvi <abavisani@me.com>
2024-06-04 17:13:16 -0400

    initial edit

--- gunfolds/scripts/GIMME_multiple_rasl.py
+++ gunfolds/scripts/GIMME_multiple_rasl.py
@@ -92,10 +92,29 @@ def read_csv_files(path,size):
                 mat = np.array(rows, dtype=np.float32)
                 matrix1 = np.array(mat[:, 0:size])
                 matrix2 = np.array(mat[:, size:2*size])
-                sum_matrix = matrix1 + matrix2
+                sum_matrix = matrix1 #+ matrix2
                 binary_matrix = (sum_matrix != 0).astype(int)
                 data.append(binary_matrix)
     return binary_matrix
+
+def read_csv_files_for_rasl(path,size):
+    files = sorted(os.listdir(path))  # Sort the files
+    for filename in files:
+        if filename.endswith('.csv'):
+            file_path = os.path.join(path, filename)
+            with open(file_path, 'r') as file:
+                csv_reader = csv.reader(file)
+                next(csv_reader)  # Skip header if exists
+                rows = []
+                for row in csv_reader:
+                    rows.append(row[0:2*size])
+                mat = np.array(rows, dtype=np.float32)
+                matrix1 = np.array(mat[:, 0:size])
+                matrix2 = np.array(mat[:, size:2*size])
+                A = (matrix1 != 0).astype(int)
+                B = (matrix2 != 0).astype(int)
+    return A.T, B.T
+
 
 for nn in [1,2,3,4,5,6]:
 
@@ -146,26 +165,27 @@ for nn in [1,2,3,4,5,6]:
         F1_C2.append(undersampled_GT['cycle']['F1'])
         edge_weights = [1, 3, 1, 3, 2]
 
-        ''' 
+
         # ###trying sRASL
-
-        nx_MVGC = gk.graph2nx(MVGC)
+        A,B = read_csv_files_for_rasl(path,len(network_GT))
+        GIMME_rasl = cv.adjs2graph(A,B)
+        nx_MVGC = gk.graph2nx(GIMME_rasl)
         two_cycle = mf.find_two_cycles(nx_MVGC)
-        DD = np.ones((len(network_GT), len(network_GT))) * 5000
+        DD = np.ones((len(network_GT), len(network_GT))) * 10000
         BD = np.ones((len(network_GT), len(network_GT))) * 10000
-        for cycle in two_cycle:
-            DD[cycle[0]-1][cycle[1]-1] = 2500
-            DD[cycle[1]-1][cycle[0]-1] = 2500
-            B[cycle[0] - 1][cycle[1] - 1] = 1
-            B[cycle[1] - 1][cycle[0] - 1] = 1
-
-        for i in range(len(network_GT)):
-            DD[i][i] = 10000
-        MVGC_bi = cv.adjs2graph(csv_data.T, B)
+        # for cycle in two_cycle:
+        #     DD[cycle[0]-1][cycle[1]-1] = 2500
+        #     DD[cycle[1]-1][cycle[0]-1] = 2500
+        #     B[cycle[0] - 1][cycle[1] - 1] = 1
+        #     B[cycle[1] - 1][cycle[0] - 1] = 1
+        #
+        # for i in range(len(network_GT)):
+        #     DD[i][i] = 10000
+        # MVGC_bi = cv.adjs2graph(csv_data.T, B)
         # gt.plotg(MVGC_bi, output='./figs/cycle_removed/Gopt_GC_' + str(nn*1000+fl) + '.pdf')
         edge_weights = [1, 3, 1, 3, 2]
-        r_estimated = drasl([MVGC_bi], weighted=True, capsize=0,
-                            urate=min(5, (3 * len(MVGC_bi) + 1)),
+        r_estimated = drasl([GIMME_rasl], weighted=True, capsize=0,
+                            urate=min(5, (3 * len(GIMME_rasl) + 1)),
                             scc=False,
                             dm=[DD],
                             bdm=[BD],
@@ -251,15 +271,14 @@ for nn in [1,2,3,4,5,6]:
 
         Precision_C5.append(least_err_sol['cycle']['precision'])
         Recall_C5.append(least_err_sol['cycle']['recall'])
-        F1_C5.append(least_err_sol['cycle']['F1'])'''
+        F1_C5.append(least_err_sol['cycle']['F1'])
 
 now = str(datetime.now())
 now = now[:-7].replace(' ', '_')
 
 ###saving files
-filename = PreFix + '_prior_'+''.join(map(str, edge_weights))+'_with_selfloop_net_' + str('all') + '_amp_' + now + '_' + (
+filename = PreFix + '_lag_only_cutoff05_prior_'+''.join(map(str, edge_weights))+'_with_selfloop_net_' + str('all') + '_amp_' + now + '_' + (
     'concat' if concat else 'individual')
-
 # Data for group 1
 data_group1 = [
     [Precision_O, Recall_O, F1_O],
@@ -274,7 +293,7 @@ data_group2 = [
     [Precision_C2, Recall_C2, F1_C2]
 ]
 
-'''data_group3 = [
+data_group3 = [
     [Precision_O3, Recall_O3, F1_O3],
     [Precision_A3, Recall_A3, F1_A3],
     [Precision_C3, Recall_C3, F1_C3]
@@ -290,7 +309,7 @@ data_group5 = [
     [Precision_O5, Recall_O5, F1_O5],
     [Precision_A5, Recall_A5, F1_A5],
     [Precision_C5, Recall_C5, F1_C5]
-]'''
+]
 
 # Labels and titles for subplots
 titles = ['Orientation', 'Adjacency', '2 cycles']
@@ -299,29 +318,29 @@ colors = ['blue', 'orange', 'red', 'yellow', 'green']
 fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(18, 5))
 
 for i, (data1, data2,
-        # data3,
-        # data4,
-        # data5,
+        data3,
+        data4,
+        data5,
         title) in enumerate(zip(data_group1, data_group2,
-                                                                   # data_group3,
-                                                                   # data_group4,
-                                                                   # data_group5,
+                                                                   data_group3,
+                                                                   data_group4,
+                                                                   data_group5,
                                                                    titles)):
     ax1 = axes[i]
-    # ax2 = ax1.twinx()
-    # ax3 = ax1.twinx()
-    # ax4 = ax1.twinx()
-    # ax5 = ax1.twinx()
+    ax2 = ax1.twinx()
+    ax3 = ax1.twinx()
+    ax4 = ax1.twinx()
+    ax5 = ax1.twinx()
     ax1.boxplot(data1, positions=np.array(range(len(data1))) * 2.0 - 0.6, patch_artist=True, showmeans=True,
                 boxprops=dict(facecolor=colors[0]), widths=0.3)
     ax1.boxplot(data2, positions=np.array(range(len(data2))) * 2.0 - 0.3, patch_artist=True, showmeans=True,
                 boxprops=dict(facecolor=colors[1]), widths=0.3)
-    # ax1.boxplot(data3, positions=np.array(range(len(data3))) * 2.0 , patch_artist=True, showmeans=True,
-    #             boxprops=dict(facecolor=colors[2]), widths=0.3)
-    # ax1.boxplot(data4, positions=np.array(range(len(data4))) * 2.0 + 0.3, patch_artist=True, showmeans=True,
-    #             boxprops=dict(facecolor=colors[3]), widths=0.3)
-    # ax1.boxplot(data5, positions=np.array(range(len(data5))) * 2.0 + 0.6, patch_artist=True, showmeans=True,
-    #             boxprops=dict(facecolor=colors[4]), widths=0.3)
+    ax1.boxplot(data3, positions=np.array(range(len(data3))) * 2.0 , patch_artist=True, showmeans=True,
+                boxprops=dict(facecolor=colors[2]), widths=0.3)
+    ax1.boxplot(data4, positions=np.array(range(len(data4))) * 2.0 + 0.3, patch_artist=True, showmeans=True,
+                boxprops=dict(facecolor=colors[3]), widths=0.3)
+    ax1.boxplot(data5, positions=np.array(range(len(data5))) * 2.0 + 0.6, patch_artist=True, showmeans=True,
+                boxprops=dict(facecolor=colors[4]), widths=0.3)
 
     ax1.set_xticks(range(0, len(data1) * 2, 2))
     ax1.set_xticklabels(['Precision', 'Recall', 'F1-score'])
@@ -341,10 +360,10 @@ red_patch = mpatches.Patch(color='red', label='MVGC+bi+sRASL')
 yellow_patch = mpatches.Patch(color='yellow', label='mean error')
 green_patch = mpatches.Patch(color='green', label='least cost sol')
 plt.legend(handles=[blue_patch, orange_patch,
-                    # red_patch,
-                    # yellow_patch,
-                    # green_patch],
-           ],loc='upper right')
+                    red_patch,
+                    yellow_patch,
+                    green_patch],
+           loc='upper right')
 
 plt.tight_layout()
 
