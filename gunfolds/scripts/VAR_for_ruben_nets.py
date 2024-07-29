02b6df4847781293892936c074dac977f4a110d3
sajadabvi <abavisani@me.com>
2024-07-29 12:06:13 -0400

    update gimme files

--- gunfolds/scripts/VAR_for_ruben_nets.py
+++ gunfolds/scripts/VAR_for_ruben_nets.py
@@ -43,7 +43,7 @@ def parse_arguments(PNUM):
     parser.add_argument("-m", "--SCCMEMBERS", default="f",
                         help="true for using g_estimate SCC members, false for using "
                              "GT SCC members", type=str)
-    parser.add_argument("-u", "--UNDERSAMPLING", default=2, help="sampling rate in generated data", type=int)
+    parser.add_argument("-u", "--UNDERSAMPLING", default=3, help="sampling rate in generated data", type=int)
     parser.add_argument("-x", "--MAXU", default=5, help="maximum number of undersampling to look for solution.",
                         type=int)
     parser.add_argument("-a", "--ALPHA", default=50, help="alpha_level for PC multiplied by 1000", type=int)
@@ -70,6 +70,31 @@ def initialize_metrics():
         'Precision_A': [], 'Recall_A': [], 'F1_A': [],
         'Precision_C': [], 'Recall_C': [], 'F1_C': []
     }
+
+def convert_to_mat(args):
+    data = zkl.load(f'datasets/VAR_sim_ruben_simple_net{args.NET}_undersampled_by_{args.UNDERSAMPLING}.zkl')
+    for i, dd in enumerate(data, start=1):
+        folder = f'./DataSets_Feedbacks/8_VAR_simulation/net{args.NET}/u{args.UNDERSAMPLING}/mat'
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+        savemat(folder + '/expo_to_mat_' + str(i) + '.mat', {'dd': dd})
+
+def convert_to_txt(args):
+    data = zkl.load(f'datasets/VAR_sim_ruben_simple_net{args.NET}_undersampled_by_{args.UNDERSAMPLING}.zkl')
+    for i, dd in enumerate(data, start=1):
+        folder = f'./DataSets_Feedbacks/8_VAR_simulation/net{args.NET}/u{args.UNDERSAMPLING}/txt'
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+        header = '\t'.join([f'X{j + 1}' for j in range(dd.shape[0])])
+
+        with open(f'{folder}/data{i}.txt', 'w') as f:
+            # Write the header
+            f.write(header + '\n')
+
+            # Write the data, one column per line
+            for col in range(dd.shape[1]):
+                line = '\t'.join(map(str, dd[:, col]))
+                f.write(line + '\n')
 
 def run_analysis(args):
     metrics = {key: initialize_metrics() for key in ['MVGC', 'MVAR', 'GIMME', 'FASK', 'RASL', 'mRASL']}
@@ -116,10 +141,16 @@ if __name__ == "__main__":
     args = parse_arguments(PNUM)
     args = convert_str_to_bool(args)
     omp_num_threads = args.PNUM
-    os.environ['OMP_NUM_THREADS'] = omp_num_threads
+    os.environ['OMP_NUM_THREADS'] = str(omp_num_threads)
 
     pattern = f'datasets/VAR_sim_ruben_simple_net{args.NET}_undersampled_by_{args.UNDERSAMPLING}.zkl'
 
     if not glob.glob(pattern):
         save_dataset(args)
-    run_analysis(args)
+    for i in range(1,10):
+        for j in range(1,4):
+            print(f'net {i}, u {j}')
+            args.NET = i
+            args.UNDERSAMPLING = j
+            convert_to_txt(args)
+    # run_analysis(args)
