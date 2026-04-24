8d0af1f6f9bc803e282273296a3398a66544a4e7
Mohammadsajad Abavisani <abavisani@me.com>
2026-04-24 15:06:30 -0400

    setup fix

--- setup.py
+++ setup.py
@@ -13,8 +13,7 @@ epoints = """
 
 install_requires = ['networkx', 'igraph', 'scipy', 'numpy',
                     'statsmodels', 'sympy', 'seaborn', 'pandas',
-                    'matplotlib', 'progressbar', 'ipdb', 'pydot', 'tigramite']
-                    'matplotlib', 'progressbar', 'ipdb', 'pydot', 'clingo']
+                    'matplotlib', 'progressbar', 'ipdb', 'pydot', 'tigramite', 'clingo']
 
 tests_require = install_requires
 
